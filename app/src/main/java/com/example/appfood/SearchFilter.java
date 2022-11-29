ae7e70790b15c03068b9794de33837bc8a3cd795
ayaN022 <aya.nadif4@gmail.com>
2022-11-29 18:03:51 +0100

    add filter layout

---
+++ app/src/main/java/com/example/appfood/SearchFilter.java
@@ -0,0 +1,13 @@
+package com.example.appfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchFilter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_filter);
+    }
+}
